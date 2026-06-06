0d9c069e97e8f7576237f3113fe88b34bb118cfc
Enio Jr <eniojr100@gmail.com>
2026-06-06 00:28:28 -0300

    Rota secreta inline para garantir acesso ao admin

--- back/sigeo_core/urls.py
+++ back/sigeo_core/urls.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
-from eventos.views import criar_admin_emergencia
+from django.http import HttpResponse
+from django.contrib.auth import get_user_model
+
+# 2. Cole a função direto aqui, antes das rotas
+def criar_admin_emergencia(request):
+    User = get_user_model()
+    if not User.objects.filter(username='admin_salvador').exists():
+        User.objects.create_superuser('admin_salvador', 'admin@email.com', 'SenhaSalva123')
+        return HttpResponse("<h1>SUCESSO! 🎉</h1> <p>Pode ir no painel /admin e logar com:</p> <p><b>Usuário:</b> admin_salvador</p> <p><b>Senha:</b> SenhaSalva123</p>")
+    
+    return HttpResponse("O admin_salvador já foi criado! Vá fazer o login.")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
